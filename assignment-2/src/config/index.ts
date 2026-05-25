46788f51e889a0d61da56c7f5f67417fe680c1ec
Chinmoy Sarkar <chinmoy4907@gmail.com>
2026-05-25 17:46:27 +0600

    setup postgresql database schema and connection

--- assignment-2/src/config/index.ts
+++ assignment-2/src/config/index.ts
@@ -4,5 +4,6 @@ dotenv.config({ path: path.join(process.cwd(),".env") });
 
 const config = {
   port : process.env.PORT,
+  database_connection_string : process.env.DATABASE_CONNECTION_STRING
 };
 export default config;
--- assignment-2/src/server.ts
+++ assignment-2/src/server.ts
@@ -1,7 +1,9 @@
 import app from "./app.js";
 import config from "./config/index.js";
+import { initDB } from "./db/index.js";
 
 const main = () => {
+  initDB();
   app.listen(config.port, () => {
     console.log(`Server is running on http://localhost:${config.port}`);
   });
