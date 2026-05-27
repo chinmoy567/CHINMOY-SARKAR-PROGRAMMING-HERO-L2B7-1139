42a415614e1230b9b4d8ae2afa71aa44cf4e897b
Chinmoy Sarkar <chinmoy4907@gmail.com>
2026-05-27 21:02:29 +0600

    Implement issue retrieval functionality: add getIssues and getSingleIssue methods, update routes and service

--- assignment-2/src/modules/issues/issues.controller.ts
+++ assignment-2/src/modules/issues/issues.controller.ts
@@ -1,9 +1,13 @@
 import type { NextFunction, Request, Response } from "express";
 import { StatusCodes } from "http-status-codes";
-
 import sendResponse from "../../utils/sendResponse";
-import { createIssueIntoDB } from "./issues.service";
+import {
+  createIssueIntoDB,
+  getIssuesFromDB,
+  getSingleIssueFromDB,
+} from "./issues.service";
 
+// create issue
 export const createIssue = async (
   req: Request,
   res: Response,
@@ -17,6 +21,48 @@ export const createIssue = async (
       StatusCodes.CREATED,
       true,
       "Issue created successfully",
+      result,
+    );
+  } catch (error) {
+    next(error);
+  }
+};
+
+// get all issues
+export const getIssues = async (
+  req: Request,
+  res: Response,
+  next: NextFunction,
+) => {
+  try {
+    const result = await getIssuesFromDB(req.query);
+
+    sendResponse(
+      res,
+      StatusCodes.OK,
+      true,
+      "Issues retrieved successfully",
+      result,
+    );
+  } catch (error) {
+    next(error);
+  }
+};
+
+// get single issue
+export const getSingleIssue = async (
+  req: Request,
+  res: Response,
+  next: NextFunction,
+) => {
+  try {
+    const result = await getSingleIssueFromDB(Number(req.params.id));
+
+    sendResponse(
+      res,
+      StatusCodes.OK,
+      true,
+      "Issue retrieved successfully",
       result,
     );
   } catch (error) {
--- assignment-2/src/modules/issues/issues.routes.ts
+++ assignment-2/src/modules/issues/issues.routes.ts
@@ -1,10 +1,12 @@
 import express from "express";
 
 import auth from "../../middleware/auth";
-import { createIssue } from "./issues.controller";
+import { createIssue, getIssues, getSingleIssue } from "./issues.controller";
 
 const router = express.Router();
 
 router.post("/", auth(), createIssue);
+router.get("/", getIssues);
+router.get("/:id", getSingleIssue);
 
 export default router;
